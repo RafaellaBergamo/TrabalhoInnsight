aeb3eca2122a7cc7d52855ebc7b49487c6d2f15a
RafaellaBergamo <rafaellabergamo3@gmail.com>
2023-11-11 19:02:33 -0300

    Criado action de cadastro de reserva

--- app/Http/Controllers/ReservasController.php
+++ app/Http/Controllers/ReservasController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reserva;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ReservasController extends Controller
 {
+    public function cadastrarReserva(Request $request) 
+    {
+        try {
+            $request->validate([
+                'idHotel' => 'required|integer', 
+                'idHospede' => 'required|integer',
+                'idQuarto' => 'required|integer',
+                'dtEntrada' => 'required|date',
+                'dtSaida' => 'required|date',
+                'vlSaida' => 'required|float'
+            ]);
+
+            Reserva::create($request->all());
     
+            return response()->json(["message" => "Reserva cadastrada com sucesso!"], 201);
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->errors()], 422);
+        }
+    }
 }
