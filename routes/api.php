77929824386dd47b25196df600b3655d96d177df
RafaellaBergamo <rafaellabergamo3@gmail.com>
2023-11-11 19:16:17 -0300

    Criado rota que cadastra hóspede

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HoteisController;
 use App\Http\Controllers\QuartosController;
 use App\Http\Controllers\ReservasController;
+use App\Http\Controllers\HospedeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,5 +26,7 @@ Route::post("/quartos", [QuartosController::class, 'cadastrarQuarto']);
 Route::get("/quartos", [QuartosController::class, 'buscarQuartos']);
 Route::get("/quartos/status", [QuartosController::class, 'verificarStatusQuarto']);
 Route::put('/quartos', [QuartosController::class, 'atualizarQuarto']);
+
+Route::post('/hospedes', [HospedeController::class, 'cadastrarHospede']);
 
 Route::post("/reservas", [ReservasController::class, 'cadastrarReserva']);
