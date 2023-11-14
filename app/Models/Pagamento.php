22d0d9f348731ba40c17ddfd8d3d0b16d3b5ab58
RafaellaBergamo <rafaellabergamo3@gmail.com>
2023-11-14 00:28:06 -0300

    uteis de pagamento

--- app/Models/Pagamento.php
+++ app/Models/Pagamento.php
@@ -20,4 +20,58 @@ class Pagamento extends Model
         'dtPagamento',
         'formaPagamento'
     ];
+
+    /**
+     * Veriica se o pagamento da reserva foi efetuado
+     * 
+     * @param int $idReserva
+     * @return bool
+     */
+    public static function pagamentoPendente(int $idReserva): bool
+    {
+        $pagamento = Pagamento::query()
+            ->where('idReserva', '=', $idReserva)
+            ->where('dtPagamento', '=', null)
+            ->count();
+
+        return !empty($pagamento);
+    }
+
+    /**
+     * Gera um registro na tabela Pagamentos referente a reserva
+     * 
+     * @param int $idHospede
+     * @param int $idReserva
+     * 
+     * @return void
+     */
+    public static function gerarPagamentoPendente(int $idHospede, int $idReserva) 
+    {
+        Pagamento::create([
+            'idHospede' => $idHospede,
+            'idReserva' => $idReserva
+        ]);
+    }
+
+    /**
+     * Verifica se a forma de pagamento escolhida pelo cliente é inválida
+     * 
+     * @param string $formaPagamento
+     * @return bool
+     */
+    public static function formaPagamentoValida(string $formaPagamento): bool
+    {
+        $formasValidas = [
+            'BOLETO' => self::BOLETO,
+            'CARTAO_CREDITO' => self::CARTAO_CREDITO,
+            'CARTAO_DEBITO' => self::CARTAO_DEBITO,
+            'DINHEIRO' => self::DINHEIRO
+        ];
+
+        if (!isset($formasValidas[$formaPagamento])) {
+            return false;
+        }
+
+        return true;
+    }
 }
