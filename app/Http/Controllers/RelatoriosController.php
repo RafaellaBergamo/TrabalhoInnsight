fb1ceae399b44b329cbc783576adf4b02fb2bd8d
RafaellaBergamo <rafaellabergamo3@gmail.com>
2023-11-26 17:04:20 -0300

    Criado novo relatório

--- app/Http/Controllers/RelatoriosController.php
+++ app/Http/Controllers/RelatoriosController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Helpers\FuncionariosHelper;
 use App\Helpers\HospedesHelper;
+use App\Helpers\ProdutosHelper;
 use App\Models\Funcionario;
+use App\Models\Hotel;
 use Barryvdh\DomPDF\Facade\Pdf as FacadePdf;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -15,7 +17,7 @@ use Illuminate\Validation\ValidationException;
 class RelatoriosController extends Controller
 {
     /**
-     * Cadastra um quarto
+     * Gera um relatório de todos os hóspedes atuais do Hotel
      * 
      * @param Request $request
      */
@@ -52,4 +54,43 @@ class RelatoriosController extends Controller
         }
     }
 
-}
+    /**
+     * Gera um relatório de produtos
+     * 
+     * @param Request $request
+     */
+    public function gerarRelatorioProdutos(Request $request)
+    {
+        try {
+            $request->validate([
+                'emailFuncionario' => 'required|email',
+                'senhaFuncionario' => 'required',
+                'idHotel' => 'integer',
+                'idProduto' => 'integer',
+                'descricao' => 'string'
+            ]);
+
+            $email = $request->input('emailFuncionario');
+            $senha = $request->input('senhaFuncionario');
+
+            if (!FuncionariosHelper::funcionarioComAcesso($email, $senha, [Funcionario::MASTER])) {
+                throw new Exception("Funcionário sem acesso.");
+            }
+
+            $idHotel = $request->input('idHotel');
+            $idProduto = $request->input('idProduto');
+            $descricao = $request->input('descricao');
+
+            $produtos = ProdutosHelper::buscarProdutos($idProduto, $idHotel, (string) $descricao);
+
+            $relatorio = FacadePdf::loadView('relatorios.relatorioProdutos', ["produtos" => $produtos]);
+
+            return response($relatorio->output(), 200)
+                ->header('Content-Type', 'application/pdf');
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
